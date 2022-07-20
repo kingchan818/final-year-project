02e962a314135023bde37614f6f380e9528968bb
kingchan818 <61489623+kingchan818@users.noreply.github.com>
2022-07-20 17:29:49 +0800

    update of websocket

--- src/webSocket/redis/index.ts
+++ src/webSocket/redis/index.ts
@@ -3,7 +3,7 @@ import Redis from 'ioredis';
 const initRedis = () => {
     const redisCli = new Redis();
     redisCli.on('connect', () => {
-        console.log('user-connected');
+        console.log('reddis client connected');
     });
 
     return redisCli;
--- src/webSocket/test.ts
+++ src/webSocket/test.ts
@@ -2,25 +2,13 @@ import { Server, Socket } from 'socket.io';
 import http from 'http';
 import initRedis from './redis/index';
 import Authenticaton from './middlewares/Auth';
-import admin from 'firebase-admin';
-import * as dotenv from 'dotenv';
-dotenv.config();
+import firebaseInit from './firebase';
+import { instrument } from '@socket.io/admin-ui';
 
-const httpServer = http.createServer().listen(8080, () => {
-    console.log('listening to socket port 8080');
-});
+const httpServer = http.createServer();
 const redisClient = initRedis();
+firebaseInit();
 const io = new Server(httpServer, { cors: { origin: '*' } });
-const firebaseConfig = {
-    apiKey: process.env.FIREBASE_API_KEY,
-    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.FIREBASE_APP_ID,
-    measurementId: process.env.FIREBASE_MEASUREMENT_ID,
-};
-admin.initializeApp(firebaseConfig);
 
 io.use(async (socket: Socket, next) => {
     // every connection estliblished need JWT or NEW SESSIONTO REDIS
@@ -28,23 +16,11 @@ io.use(async (socket: Socket, next) => {
     console.log('token id', socket.handshake.auth.token);
     const auth = new Authenticaton(redisClient, socket);
     await auth.jwtWatcher();
-    // const authenticatedUser = await firebaseAuthSocket(socket.handshake.auth.token);
-    // if (!firebaseAuthSocket(socket.handshake.auth.token)) {
-    //     console.log('has been disconnected ');
-    //     socket.disconnect();
-    // }
-    // if (await !redisClient.hmget(socket.handshake.auth.token)) {
-    //     const newUser: User = { user: authenticatedUser, userConnections: [socket.id] };
-    //     redisClient.hmset(socket.handshake.auth.token, newUser);
-    // }
-    // const connections = JSON.parse((await redisClient.hget(socket.handshake.auth.token, CONNECTION)) as string);
-    // redisClient.hmset(socket.handshake.auth.token, { CONNECTION: connections.push(socket.id) });
-    // next();
     next();
 });
 
 io.on('connection', (socket: Socket) => {
-    console.log('connection established');
+    console.log('connection established...........');
     console.log('user connection id', socket.id);
     const currentUser = socket.data.currentUser;
     socket.on('private-message', (arg: { connectionId: string; msg: string }) => {
@@ -67,4 +43,11 @@ io.engine.on('connection_error', (err: any) => {
     console.log(err.code); // the error code, for example 1
     console.log(err.message); // the error message, for example "Session ID unknown"
     console.log(err.context); // some additional error context
+});
+instrument(io, {
+    auth: false,
+});
+
+httpServer.listen(8080, () => {
+    console.log('listening to socket port 8080');
 });
